331dff963ab47dda39bfb27ca633b832cea5944a
Miyirbek <69167745+Miyirbek2003@users.noreply.github.com>
2023-12-08 16:41:48 +0500

    asdasd

--- public/ckeditor/config.js
+++ public/ckeditor/config.js
@@ -3,8 +3,12 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
+CKEDITOR.editorConfig = function (config) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	
+	
+	config.extraPlugins = 'youtube';
+
 };
